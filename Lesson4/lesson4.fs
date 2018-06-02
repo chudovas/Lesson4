88458f93ea512aac20999adcd3a9879775eabfd1
Гурьев Василий Александрович <Vaschudo@gmail.com>
2018-06-02 22:34:54 +0300

    Write the project that calculates the lambda expression according to the normal strategy.

---
+++ Lesson4/lesson4.fs
@@ -0,0 +1,59 @@
+﻿open System
+
+type Term =
+    | Var of char
+    | App of T1 : Term * T2 : Term
+    | Labstraction of v : char * T : Term
+
+let Calculation (t : Term) maxNumOfCalculation =
+    let rec FV T =
+        match T with 
+        | Var(v) -> [v]
+        | App(T1, T2) -> (FV T1)@(FV T2)
+        | Labstraction(v, T1) -> List.except (FV T1) [v]
+
+    let GetZ T1 T2 =
+        let allChar = ['a'..'z']
+        let freeVar = (List.except (FV T2) (List.except (FV T1) allChar))
+        if (List.isEmpty freeVar) then
+            raise (Exception("There are no free variables!"))
+        List.head freeVar
+
+    let rec Change (term1 : Term) value (term2 : Term) =
+        match term1 with
+        | Var(v) when v = value -> term2
+        | Var(v) -> term1
+        | App(T1, T2) -> App(Change T1 value term2, Change T2 value term2)
+        | Labstraction(v, _) when (value = v) && (term2 <> Var(v)) -> term1    
+        | Labstraction(v, T) when (not (List.contains v (FV term2))) || (not (List.contains value (FV T))) -> 
+            Labstraction(v, Change T value term2)
+        | Labstraction(v, T) -> 
+            let newVar = GetZ term2 T
+            Labstraction(newVar, Change (Change T v (Var(newVar))) value term2)
+
+    let rec NormStrat t : Term =
+        match t with
+        | Var(v) -> t
+        | Labstraction(v, T) -> Labstraction(v, NormStrat T)
+        | App(Labstraction(v, T1), T2) -> Change T1 v T2
+        | App(T1, T2) -> App(NormStrat T1, NormStrat T2)
+
+    let rec recCalculation t numOfCalc =
+        if (numOfCalc = maxNumOfCalculation)
+        then None
+        elif (t = (NormStrat t))
+        then Some(t)
+        else recCalculation (NormStrat t) (numOfCalc + 1)
+    
+    recCalculation t 0
+
+module Combinators =
+    let S = (Labstraction('x', Labstraction('y', Labstraction('z', App(App(Var('x'), Var('z')), App(Var('y'), Var('z')))))))
+    let K = (Labstraction('x', Labstraction('y', Var('x'))))
+    let K' = (Labstraction('y', Labstraction('x', Var('x'))))
+    let I = (Labstraction('x', Var('x')))
+    let xToXxx = (Labstraction('x', App(App(Var('x'), Var('x')), Var('x'))))
+
+[<EntryPoint>]
+let main argv =
+    0 
---
+++ Tests/Tests.fs
@@ -0,0 +1,30 @@
+﻿open Lesson4
+open NUnit.Framework
+open FsUnit
+
+let fastCalculation X = Calculation X 100
+
+[<Test>]
+let ``test of norm strategy 1``() =
+    fastCalculation (App(Combinators.I, Var('z'))) |> should equal (Some(Var('z')))
+
+[<Test>]
+let ``test of norm strategy 2``() =
+    fastCalculation (App(Combinators.xToXxx, Combinators.xToXxx)) |> should equal null
+
+[<Test>]
+let ``test of norm strategy 3``() =
+    fastCalculation (App(Combinators.K, Combinators.I)) |> should equal (Some(Combinators.K'))
+
+[<Test>]
+let ``test of norm strategy 4``() =
+    fastCalculation (App(App(Combinators.S, Combinators.K), Combinators.K)) 
+        |> should equal (Some(Labstraction('z', Var('z'))))
+
+[<EntryPoint>]
+let main argv =
+    ``test of norm strategy 1``()
+    ``test of norm strategy 2``()
+    ``test of norm strategy 3``()
+    ``test of norm strategy 4``()
+    0 
